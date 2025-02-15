1688e00eab49d031019736fdb01cb9307fe478cb
ZErvida <97213948+zervidas@users.noreply.github.com>
2025-02-15 19:07:06 +0700

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,2 +1,7 @@
-const modules = await import('./index.mjs'); // Ambil dari ESM
-module.exports = modules.default; // Ekspor sebagai CommonJS
+async function loadModule() {
+    const modules = await import('./index.mjs'); // Ambil dari ESM
+    module.exports = modules.default; // Ekspor sebagai CommonJS
+}
+
+// Jalankan fungsi untuk load modul
+loadModule();
